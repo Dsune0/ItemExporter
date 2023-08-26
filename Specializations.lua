4c74372dc77f288b76236a9ed79dbdef1524ecb9
Dsune0 <106332935+Dsune0@users.noreply.github.com>
2023-08-26 13:37:31 +0200

    specialization use loot index

--- Specializations.lua
+++ Specializations.lua
@@ -25,71 +25,71 @@ for key, classKey in pairs(classLootIndex) do
 end
 
 ItemExporter.Specializations = {
-    ["Death Knight"] = {
+    [1] = {
+        [71] = "Arms",
+        [72] = "Fury",
+        [73] = "Protection"
+    },
+    [2] = {
+        [65] = "Holy",
+        [66] = "Protection",
+        [70] = "Retribution"
+    },
+    [3] = {
+        [253] = "Beast Mastery",
+        [254] = "Marksmanship",
+        [255] = "Survival"
+    },
+    [4] = {
+        [259] = "Assassination",
+        [260] = "Outlaw",
+        [261] = "Subtlety"
+    },
+    [5] = {
+        [256] = "Discipline",
+        [257] = "Holy",
+        [258] = "Shadow"
+    },
+    [6] = {
         [250] = "Blood",
         [251] = "Frost",
         [252] = "Unholy"
     },
-    ["Demon Hunter"] = {
-        [577] = "Havoc",
-        [581] = "Vengeance"
+    [7] = {
+        [262] = "Elemental",
+        [263] = "Enhancement",
+        [264] = "Restoration"
     },
-    ["Druid"] = {
+    [8] = {
+        [62] = "Arcane",
+        [63] = "Fire",
+        [64] = "Frost"
+    },
+    [9] = {
+        [265] = "Affliction",
+        [266] = "Demonology",
+        [267] = "Destruction"
+    },
+    [10] = {
+        [268] = "Brewmaster",
+        [270] = "Mistweaver",
+        [269] = "Windwalker"
+    },
+    [11] = {
         [102] = "Balance",
         [103] = "Feral",
         [104] = "Guardian",
         [105] = "Restoration"
     },
-    ["Evoker"] = {
+    [12] = {
+        [577] = "Havoc",
+        [581] = "Vengeance"
+    },
+    [13] = {
         [1467] = "Devastation",
         [1468] = "Preservation",
         [1473] = "Augmentation"
     },
-    ["Hunter"] = {
-        [253] = "Beast Mastery",
-        [254] = "Marksmanship",
-        [255] = "Survival"
-    },
-    ["Mage"] = {
-        [62] = "Arcane",
-        [63] = "Fire",
-        [64] = "Frost"
-    },
-    ["Monk"] = {
-        [268] = "Brewmaster",
-        [270] = "Mistweaver",
-        [269] = "Windwalker"
-    },
-    ["Paladin"] = {
-        [65] = "Holy",
-        [66] = "Protection",
-        [70] = "Retribution"
-    },
-    ["Priest"] = {
-        [256] = "Discipline",
-        [257] = "Holy",
-        [258] = "Shadow"
-    },
-    ["Rogue"] = {
-        [259] = "Assassination",
-        [260] = "Outlaw",
-        [261] = "Subtlety"
-    },
-    ["Shaman"] = {
-        [262] = "Elemental",
-        [263] = "Enhancement",
-        [264] = "Restoration"
-    },
-    ["Warlock"] = {
-        [265] = "Affliction",
-        [266] = "Demonology",
-        [267] = "Destruction"
-    },
-    ["Warrior"] = {
-        [71] = "Arms",
-        [72] = "Fury",
-        [73] = "Protection"
-    }
 }
 
 function ItemExporter:GetCurrentSpecialization()
