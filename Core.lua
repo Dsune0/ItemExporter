95d41b28a890e4169da5aebc366edcdefdfaccc7
Dsune0 <106332935+Dsune0@users.noreply.github.com>
2023-08-26 23:16:02 +0200

    fetch tiersetID

--- Core.lua
+++ Core.lua
@@ -8,29 +8,29 @@ local AceEvent = LibStub("AceEvent-3.0")
 local L = ItemExporter.L
 
 ItemExporter.db = LibStub("AceDB-3.0"):New("ItemExporterDB", {
-    profile = {
-        minimap = {
-            hide = false,
-        },
-    },
+	profile = {
+		minimap = {
+			hide = false,
+		},
+	},
 })
 
 -- Slash command
 function ItemExporter:OnInitialize()
-    self:RegisterChatCommand("itemexport", "ToggleGUI")
+	self:RegisterChatCommand("itemexport", "ToggleGUI")
 end
 
 -- Minimap button
 local dataObj = LDB:NewDataObject("ItemExporter", {
-    type = "data source",
-    text = L["ItemExporter"],
-    icon = "134332",
-    OnClick = function()
-        ItemExporter:ToggleGUI()
-    end,
-    OnTooltipShow = function(tooltip)
-        tooltip:SetText(L["ItemExporter"])
-    end,
+	type = "data source",
+	text = L["ItemExporter"],
+	icon = "134332",
+	OnClick = function()
+		ItemExporter:ToggleGUI()
+	end,
+	OnTooltipShow = function(tooltip)
+		tooltip:SetText(L["ItemExporter"])
+	end,
 })
 
 --register minimap button
@@ -38,62 +38,78 @@ icon:Register("ItemExporter", dataObj, ItemExporter.db.profile.minimap)
 
 -- reenable EJ events
 function ItemExporter:ReEnableEJ()
-  EncounterJournal:RegisterEvent("EJ_LOOT_DATA_RECIEVED");
-  EncounterJournal:RegisterEvent("EJ_DIFFICULTY_UPDATE");
-  EncounterJournal:RegisterEvent("UNIT_PORTRAIT_UPDATE");
-  EncounterJournal:RegisterEvent("PORTRAITS_UPDATED");
-  EncounterJournal:RegisterEvent("SEARCH_DB_LOADED");
-  EncounterJournal:RegisterEvent("UI_MODEL_SCENE_INFO_UPDATED");
+	EncounterJournal:RegisterEvent("EJ_LOOT_DATA_RECIEVED");
+	EncounterJournal:RegisterEvent("EJ_DIFFICULTY_UPDATE");
+	EncounterJournal:RegisterEvent("UNIT_PORTRAIT_UPDATE");
+	EncounterJournal:RegisterEvent("PORTRAITS_UPDATED");
+	EncounterJournal:RegisterEvent("SEARCH_DB_LOADED");
+	EncounterJournal:RegisterEvent("UI_MODEL_SCENE_INFO_UPDATED");
 end
 
 --disable EJ events to prevent taint
 function ItemExporter:DisableEJ()
-  EncounterJournal:UnregisterEvent("EJ_LOOT_DATA_RECIEVED");
-  EncounterJournal:UnregisterEvent("EJ_DIFFICULTY_UPDATE");
-  EncounterJournal:UnregisterEvent("UNIT_PORTRAIT_UPDATE");
-  EncounterJournal:UnregisterEvent("PORTRAITS_UPDATED");
-  EncounterJournal:UnregisterEvent("SEARCH_DB_LOADED");
-  EncounterJournal:UnregisterEvent("UI_MODEL_SCENE_INFO_UPDATED");
+	EncounterJournal:UnregisterEvent("EJ_LOOT_DATA_RECIEVED");
+	EncounterJournal:UnregisterEvent("EJ_DIFFICULTY_UPDATE");
+	EncounterJournal:UnregisterEvent("UNIT_PORTRAIT_UPDATE");
+	EncounterJournal:UnregisterEvent("PORTRAITS_UPDATED");
+	EncounterJournal:UnregisterEvent("SEARCH_DB_LOADED");
+	EncounterJournal:UnregisterEvent("UI_MODEL_SCENE_INFO_UPDATED");
 end
 
 
 -- Collect instance info
 function ItemExporter:GetLatestContentInfo()
 	ItemExporter:DisableEJ()
-    local latestTierIndex = EJ_GetNumTiers()
+	local latestTierIndex = EJ_GetNumTiers()
 	EJ_SelectTier(latestTierIndex)
-    
-    local raids = {}
-    local dungeons = {}
-
+	
+	local raids = {}
+	local dungeons = {}
+	local tierset = {}
+	
 	-- Collect raids
-    local index = 1
-    while true do
-        local instanceID, name, _, _, _, _, _, _, _, isRaid = EJ_GetInstanceByIndex(index, true)
-        if not instanceID then break end
-        if isRaid then
+	local index = 1
+	while true do
+		local instanceID, name, _, _, _, _, _, _, _, isRaid = EJ_GetInstanceByIndex(index, true)
+		if not instanceID then break end
+		if isRaid then
 			local bosses = {}
-            EJ_SelectInstance(instanceID)
+			EJ_SelectInstance(instanceID)
 			local bossIndex = 1
-            while true do
-                local bossName, _, encounterID = EJ_GetEncounterInfoByIndex(bossIndex)
-                if not bossName then break end		
-                table.insert(bosses, {name = bossName, encounterID = encounterID})
-                bossIndex = bossIndex + 1
-            end
-            table.insert(raids, {instanceName = name, instanceID = instanceID, bosses = bosses})
-        end
-        index = index + 1
-    end
-    
-    -- Collcet mythic+ dungeons
-    index = 1
-    while true do
-        local instanceID, name = EJ_GetInstanceByIndex(index, false)
-        if not instanceID then break end
-        table.insert(dungeons, {instanceName = name, instanceID = instanceID})
-        index = index + 1
-    end
+			while true do
+				local bossName, _, encounterID = EJ_GetEncounterInfoByIndex(bossIndex)
+				if not bossName then break end		
+				table.insert(bosses, {name = bossName, encounterID = encounterID})
+				bossIndex = bossIndex + 1
+			end
+			table.insert(raids, {instanceName = name, instanceID = instanceID, bosses = bosses})
+		end
+		index = index + 1
+	end
+	
+	-- Collect mythic+ dungeons
+	index = 1
+	while true do
+		local instanceID, name = EJ_GetInstanceByIndex(index, false)
+		if not instanceID then break end
+		table.insert(dungeons, {instanceName = name, instanceID = instanceID})
+		index = index + 1
+	end
+	
+	-- Collect tierset ID
+	for _,info in pairs(C_TransmogSets.GetBaseSets()) do
+		for _,raid in ipairs(raids) do		
+			if info.label and raid.instanceName and info.label == raid.instanceName then
+				tierset.setID = info.setID
+				tierset.name = info.name
+				tierset.label = L["Tierset"]
+			end
+		end
+	end
+	
 	ItemExporter:ReEnableEJ()
-    return raids, dungeons
+	return raids, dungeons, tierset
 end
+
+
+
