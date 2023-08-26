ab402c5d2b35b1388cbc68fbfa018d4ccf50b33e
Dsune0 <106332935+Dsune0@users.noreply.github.com>
2023-08-26 23:16:26 +0200

    generic toggle all locale

--- Localization.lua
+++ Localization.lua
@@ -11,8 +11,8 @@ L["ItemExport"] = "ItemExport"
 L["ItemExporter"] = "ItemExporter"
 L["Export itemstrings to SimulationCraft format"] = "Export itemstrings to SimulationCraft format"
 L["Export"] = "Export"
-L["Toggle All Instances"] = "Toggle All Instances"
-L["Toggle All Armortypes"] = "Toggle All Armortypes"
+L["Toggle All"] = "Toggle All"
+L["Tierset"] = "Tierset"
 
 -- German (deDE)
 if GetLocale() == "deDE" then
@@ -20,8 +20,7 @@ if GetLocale() == "deDE" then
 	L["ItemExporter"] = "ItemExporter"
 	L["Export itemstrings to SimulationCraft format"] = "Itemstrings ins SimulationCraft-Format exportieren"
 	L["Export"] = "Exportieren"
-	L["Toggle All Instances"] = "Alle Instanzen umschalten"
-	L["Toggle All Armortypes"] = "Alle Rüstungstypen umschalten"
+	L["Toggle All"] = "Alles umschalten"
 end
 
 -- French (frFR)
@@ -30,8 +29,7 @@ if GetLocale() == "frFR" then
 	L["ItemExporter"] = "ItemExporter"
 	L["Export itemstrings to SimulationCraft format"] = "Exportation des chaînes de caractères au format SimulationCraft"
 	L["Export"] = "Exportation"
-	L["Toggle All Instances"] = "Basculer toutes les instances"
-	L["Toggle All Armortypes"] = "Basculer tous les types d'armures"
+	L["Toggle All"] = "Basculer tout"
 end
 
 -- Spanish (esES and esMX)
@@ -40,8 +38,7 @@ if GetLocale() == "esES" or GetLocale() == "esMX" then
 	L["ItemExporter"] = "ItemExporter"
 	L["Export itemstrings to SimulationCraft format"] = "Exportar cadenas de elementos a formato SimulationCraft"
 	L["Export"] = "Exportar"
-	L["Toggle All Instances"] = "Conmutar todas las instancias"
-	L["Toggle All Armortypes"] = "Conmutar todos los tipos de armadura"
+	L["Toggle All"] = "Conmutar todo"
 end
 
 -- Russian (ruRU)
@@ -50,8 +47,7 @@ if GetLocale() == "ruRU" then
 	L["ItemExporter"] = "ItemExporter"
 	L["Export itemstrings to SimulationCraft format"] = "Экспорт строк элементов в формат SimulationCraft"
 	L["Export"] = "Экспорт"
-	L["Toggle All Instances"] = "Переключить все экземпляры"
-	L["Toggle All Armortypes"] = "Переключить все типы брони"
+	L["Toggle All"] = "Переключить все"
 end
 
 -- Korean (koKR)
@@ -60,8 +56,7 @@ if GetLocale() == "koKR" then
 	L["ItemExporter"] = "아이템 내보내기"
 	L["Export itemstrings to SimulationCraft format"] = "아이템스트링을 SimulationCraft 형식으로 내보내기"
 	L["Export"] = "내보내기"
-	L["Toggle All Instances"] = "모든 인스턴스 토글"
-	L["Toggle All Armortypes"] =  "모든 무장 유형 토글"
+	L["Toggle All"] = "모두 토글"
 end
 
 --  Chinese Simplified (zhCN)
@@ -70,8 +65,7 @@ if GetLocale() == "zhCN" then
 	L["ItemExporter"] = "项目导出器"
 	L["Export itemstrings to SimulationCraft format"] = "将项目字符串导出为 SimulationCraft 格式"
 	L["Export"] = "出口"
-	L["Toggle All Instances"] = "切换所有实例"
-	L["Toggle All Armortypes"] = "切换所有装甲类型"
+	L["Toggle All"] = "全部切换"
 end
 
 -- Chinese Traditional (zhTW)
@@ -85,8 +79,7 @@ if GetLocale() == "itIT" then
 	L["ItemExporter"] = "ItemExporter"
 	L["Export itemstrings to SimulationCraft format"] = "Esportazione di itemstrings in formato SimulationCraft"
 	L["Export"] = "Esporta"
-	L["Toggle All Instances"] = "Attiva tutte le istanze"
-	L["Toggle All Armortypes"] = "Attiva tutti i tipi di armatura"
+	L["Toggle All"] = "Seleziona tutto"
 end
 
 -- Portuguese (ptBR)
@@ -95,6 +88,5 @@ if GetLocale() == "ptBR" then
 	L["ItemExporter"] = "Esportatore di articoli"
 	L["Export itemstrings to SimulationCraft format"] = "Exportar itens para o formato SimulationCraft"
 	L["Export"] = "Exportar"
-	L["Toggle All Instances"] = "Alternar todas as instâncias"
-	L["Toggle All Armortypes"] = "Alternar todos os tipos de armadura"
+	L["Toggle All"] = "Alternar tudo"
 end
